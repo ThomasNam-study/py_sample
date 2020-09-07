33ed36ff8c7080bc407963e5d58007d14abeaa22
RED <purred@naver.com>
2020-09-07 18:22:36 +0900

    예외 처리... 샘플 추가

---
+++ base/py_except.py
@@ -0,0 +1,16 @@
+# ZeroDivisionError
+# IndexError
+# KeyError
+# AttributeError
+# ValueError
+# FileNotFoundError
+# TypeError
+
+x = [10, 20, 30]
+
+try:
+    dic = {}
+
+    print(dic["test"])
+except ValueError:
+    print("VALUE ERROR")
--- sqliteSample/sqliteTest.py
+++ sqliteSample/sqliteTest.py
@@ -16,11 +16,11 @@ c = conn.cursor()
 
 print('Cursor type : ', type(c))
 
-#c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username text, email text, phone text, website text, regdate text)")
+# c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username text, email text, phone text, website text, regdate text)")
 
-#c.execute("INSERT INTO users VALUES(1, 'Kim', 'kim@naver.com', '010-0000-0000', 'Kim.com', ?)", (nowDateTime,))
+# c.execute("INSERT INTO users VALUES(1, 'Kim', 'kim@naver.com', '010-0000-0000', 'Kim.com', ?)", (nowDateTime,))
 
-#c.execute("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", (2, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime))
+# c.execute("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", (2, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime))
 
 userList = (
     (3, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime),
@@ -28,26 +28,24 @@ userList = (
     (5, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime)
 )
 
-#c.executemany("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", userList)
+# c.executemany("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", userList)
 
-#print(c.execute("DELETE FROM users").rowcount)
+# print(c.execute("DELETE FROM users").rowcount)
 
-#conn.commit()
-#conn.rollback()
+# conn.commit()
+# conn.rollback()
 
-#c.execute("SELECT * FROM users")
+# c.execute("SELECT * FROM users")
 
-#print("One -> ", c.fetchone())
-#print("One -> ", c.fetchmany(3))
-#print(c.fetchall())
-#rows = c.fetchall()
+# print("One -> ", c.fetchone())
+# print("One -> ", c.fetchmany(3))
+# print(c.fetchall())
+# rows = c.fetchall()
 
-#for r in rows:
+# for r in rows:
 #    print(r)
 
 for r in c.execute("SELECT * FROM users WHERE id=:Id", {"Id": 5}):
     print(r)
-
-
 
 conn.close()
