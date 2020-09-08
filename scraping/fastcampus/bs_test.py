a337c5e4518a82141582108271b0b5fd48574ab1
RED <purred@naver.com>
2020-09-08 16:51:55 +0900

    뷰티플숲 테스트

--- scraping/fastcampus/bs_test.py
+++ scraping/fastcampus/bs_test.py
@@ -19,4 +19,72 @@ html = """
 </html>
 """
 
+# bs4 초기화
+soup = BeautifulSoup(html, 'html.parser')
 
+# 타입 확인
+print('soup', type(soup))
+
+# 코드 정리
+print('prettify', soup.prettify())
+
+# h1 태그 접근
+h1 = soup.html.body.h1
+print('h1', h1)
+
+# p 태그 접근
+p1 = soup.html.body.p
+print('p1', p1)
+
+# 다음 태그
+p2 = p1.next_sibling.next_sibling
+print('p2', p2)
+
+# 텍스트 출력1
+print("h1 >> ", h1.string)
+
+# 텍스트 출력2
+print("p >> ", p1.string)
+
+# 함수 확인
+print(dir(p2))
+
+# 다음 엘리먼트 확인
+print(list(p2.next_elements))
+
+# 반복 출력 확인
+# for v in p2.next_elements:
+#    print(v)
+
+
+soup2 = BeautifulSoup(html, 'html.parser')
+
+link1 = soup2.find_all('a', limit=2)
+print('links', link1)
+
+# id = "link2", string="title", string=["EEEE"]
+link2 = soup2.find_all('a', class_='sister')
+print('links', link2)
+
+for t in link2:
+    print(t)
+
+# 처음 발견한 a 태그 선택
+link3 = soup2.find("a")
+
+print()
+print(link3)
+
+# 다중 조건
+link4 = soup2.find("a", {"class": "sister", "data-io": "link3"})
+print(link4)
+
+# css 선택자
+# select, select_one
+
+link5 = soup2.select_one('p.title > b')
+print(link5)
+print(link5.text)
+
+link6 = soup2.select_one("a#link1")
+print(link6)
