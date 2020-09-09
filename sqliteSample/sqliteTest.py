738f65ef0fafbf4277ba830ded13ecff754ff6dd
ThomasNam <purred@naver.com>
2020-09-09 21:22:20 +0900

    크롤링 추가

--- sqliteSample/sqliteTest.py
+++ sqliteSample/sqliteTest.py
@@ -10,23 +10,26 @@ print('nowDatetime : ', nowDateTime)
 print('sqlite3.version : ', sqlite3.version)
 print('sqlite3.sqlite_version : ', sqlite3.sqlite_version)
 
+# isolation_level - Auto Commit 지원
 conn = sqlite3.connect('./database.db', isolation_level=None)
 
 c = conn.cursor()
 
 print('Cursor type : ', type(c))
 
+# 테이블 생성, TEXT, NUMERIC, INTEGER, REAL, BLOB)
 # c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username text, email text, phone text, website text, regdate text)")
 
 # c.execute("INSERT INTO users VALUES(1, 'Kim', 'kim@naver.com', '010-0000-0000', 'Kim.com', ?)", (nowDateTime,))
 
 # c.execute("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", (2, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime))
 
-userList = (
-    (3, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime),
-    (4, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime),
-    (5, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime)
-)
+# Many 삽입 (튜플, 리스트)
+# userList = (
+#     (3, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime),
+#     (4, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime),
+#     (5, "Park", "Park@daum.net", '01053496254', 'http://www.naver.com', nowDateTime)
+# )
 
 # c.executemany("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", userList)
 
@@ -38,7 +41,7 @@ userList = (
 # c.execute("SELECT * FROM users")
 
 # print("One -> ", c.fetchone())
-# print("One -> ", c.fetchmany(3))
+# print("One -> ", c.fetchmany(size=3))
 # print(c.fetchall())
 # rows = c.fetchall()
 
